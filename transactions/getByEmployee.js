7e85622b279617b477f06784eb74e6b5c92623e4
unknown <jill.ba00@gmail.com>
2021-06-17 14:40:26 +0000

    add getByEmployeeId endpoint

---
+++ transactions/getByEmployee.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.get = (event, context, callback) => {
+  const params = {
+    TableName: "Transactions",
+    Key: {
+      employee_id: event.pathParameters.employee_id,
+    },
+  };
+
+  // fetch transaction from the database
+  dynamoDb.scan(params, (error, result) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t fetch the transaction items.',
+      });
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result.Items),
+    };
+    callback(null, response);
+  });
+};
